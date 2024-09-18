90ee253be0a2e2cf08b9fbd6e5e576385070a71a
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2024-09-18 20:37:48 -0300

    raw: Updating the 'Home' page component by adding the communities section to the page and a form to register a new community, using React's state concept through the 'useState' hook, creating a state to store the list of registered communities

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,45 @@
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { useState } from 'react'
+
+import {
+  AlurakutMenu,
+  AlurakutProfileSidebarMenuDefault,
+  OrkutNostalgicIconSet
+} from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar({ githubUser }) {
   return (
-    <Box>
+    <Box as="aside">
       <img
         src={`https://github.com/${githubUser}.png`}
         style={{ borderRadius: '8px' }}
       />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${githubUser}`}>
+          @{githubUser}
+        </a>
+      </p>
+
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
-  const githubUser = 'GabrielRARodrigues'
+  const [comunidades, setComunidades] = useState([
+    {
+      id: '1242345346455645573463643 ',
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+    }
+  ])
 
+  const githubUser = 'GabrielRARodrigues'
   const pessoasFavoritas = [
     'juunegreiros',
     'omariosouto',
@@ -25,6 +48,23 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho'
   ]
+
+  function handleCreateCommunity(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+
+    const comunidade = {
+      id: new Date().toISOString(),
+      title: formData.get('title'),
+      image: formData.get('image')
+    }
+
+    setComunidades(comunidades => {
+      const comunidadesAtualizadas = [...comunidades, comunidade]
+
+      return comunidadesAtualizadas
+    })
+  }
 
   return (
     <>
@@ -38,14 +78,52 @@ export default function Home() {
             <h1 className="title">Bem vindo(a)</h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateCommunity}>
+              <div>
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Qual vai ser o nome da sua comunidade?"
+                  aria-label="Qual vai ser o nome da sua comunidade?"
+                />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="image"
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <button>Criar comunidade</button>
+            </form>
+          </Box>
         </div>
         <div
           className="profileRelationsArea"
           style={{ gridArea: 'profileRelationsArea' }}
         >
           <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Comunidades ({comunidades.length})</h2>
+            {
+              <ul>
+                {comunidades.map(comunidade => (
+                  <li key={comunidade.id}>
+                    <a href={`/users/${comunidade.title}`}>
+                      <img src={comunidade.image} />
+                      <span>{comunidade.title}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            }
+          </ProfileRelationsBoxWrapper>
+
+          <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
-              Comunidade ({pessoasFavoritas.length})
+              Pessoas da comunidade ({pessoasFavoritas.length})
             </h2>
             <ul>
               {pessoasFavoritas.map(pessoa => (
